de5de6b9d7c4d1cb82267c86e568aa537c54322a
ChanMyaeOo-Dev <chanmyaeoodev@gmail.com>
2023-05-06 01:58:14 +0630

    last:

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
     <h1 style="color:red;">Home Page</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse nisi quam nostrum recusandae velit nulla laboriosam magnam exercitationem sit! Vel sequi recusandae illo ex, perferendis maiores eaque officia adipisci natus.</p>
 
+    Ok Par
+
 </body>
 
 </html>
